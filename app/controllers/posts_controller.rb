7e44e8d2fdfd2f512b6302b6b6e74dc35e188f27
Lexi Sandoval <lexi.sandoval9@gmail.com>
2021-03-19 12:24:19 -0500

    commit tree 12ef75834e3261efebd204081a3e46c30bb963ca

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,11 +6,13 @@ class PostsController < ApplicationController
 
   def edit
   end
-
+  
   def update
-    @post.update(post_params)
-
-    redirect_to post_path(@post)
+    if @post.update(post_params)
+      redirect_to post_path(@post)
+    else 
+      render :edit
+    end
   end
 
   private
